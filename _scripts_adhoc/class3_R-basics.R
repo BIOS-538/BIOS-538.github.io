0f9b274bc21cc0453e950b03b67b30d56d317816
ppreshant <ppresh.ant@gmail.com>
2024-01-17 03:03:55 -0600

    script for class03_work in progress

---
+++ _scripts_adhoc/class3_R-basics.R
@@ -0,0 +1,35 @@
+# Class3_R-basics.R
+
+# Note: Everything after the "#" is a human readable comment that R ignores
+
+
+# Simple datatypes ----
+# Note: The 4 dashes make a new section, check outline button on the top right of the script pane
+# two hashes ## with the 4 dashes make a subsection!
+
+
+## Numbers ----
+
+x <- 2.5 # assign a number to variable x
+
+# complete comments on each of the following lines after running them
+class(x)
+
+is.integer(x)
+
+### numeric operations ----
+
+2 + 3 # addition
+# try exponent (^), and remainder (%%) operators
+
+# R reads these operations like any other function: f(x, y)
+`+`(2, 3)
+`>`(2, 3)
+
+
+# TIP: Use up and down arrows in the "Console" to move around in your history
+
+
+# Your turn now:
+# a) Add today's month plus day plus year
+# b) Multiply the number of letters in the alphabet by 5
