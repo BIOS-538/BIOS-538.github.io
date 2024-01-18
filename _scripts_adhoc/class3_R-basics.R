236d6ba1694100b6e912232b17f2ad050b9a1a47
ppreshant <ppresh.ant@gmail.com>
2024-01-18 01:05:29 -0600

    updating slides and material for class03

--- _scripts_adhoc/class3_R-basics.R
+++ _scripts_adhoc/class3_R-basics.R
@@ -2,34 +2,312 @@
 
 # Note: Everything after the "#" is a human readable comment that R ignores
 
+# FOLLOW ALONG:
+# Instructions.
+# 1. Type the commands your self into the console 
+# ...(don't copy paste, typing makes you understand better)
+# 2. Run it and write a comment next to the command in this script
+# ... to explain what the command does (first example provided in line 18)
 
 # Simple datatypes ----
-# Note: The 4 dashes make a new section, check outline button on the top right of the script pane
-# two hashes ## with the 4 dashes make a subsection!
+# Note: The 4 dashes make a new section, 
+# ..... check outline button on the top right of the script pane
+# two hashes ## with the 4 dashes make a subsection, 
+# .. and so on for a structured outline!
 
 
 ## Numbers ----
 
+# write any number, it will appear as the output
+
+35
+
+# if you assign it to a variable, the value will be stored for later use
 x <- 2.5 # assign a number to variable x
 
-# complete comments on each of the following lines after running them
+# to show the output type 
+x
+
+# YOUR TURN: complete comments on each of the following lines after running them
 class(x)
 
+# Checking type
+is.numeric(x)
 is.integer(x)
 
-### numeric operations ----
+### Numeric operations ----
 
 2 + 3 # addition
 # try exponent (^), and remainder (%%) operators
 
-# R reads these operations like any other function: f(x, y)
+# SIDE NOTE: R reads these operations like any other function: f(x, y)
 `+`(2, 3)
 `>`(2, 3)
+`<-`(g, 4)
 
 
 # TIP: Use up and down arrows in the "Console" to move around in your history
 
 
 # Your turn now:
-# a) Add today's month plus day plus year
+# a) Add today's month plus day plus year; save it as variable: date_sum
 # b) Multiply the number of letters in the alphabet by 5
+
+## Logical / binary datatype ----
+
+l1 <- TRUE
+
+# This is also a logical type
+l2 <- x > 3
+# Essentially, you are asking R logical questions requiring TRUE / FALSE responses
+
+# check the type 
+is.logical(l2)
+
+
+### Logical operations
+
+# YOUR TURN NOW
+# what do the "==" and "!=" do?
+x == 2.5
+4 != 4
+
+# Operating on two logical values
+TRUE & FALSE
+TRUE | FALSE
+
+
+# type coercion/ conversion
+as.logical(x)
+as.logical(0)
+
+
+# REFERENCE: Logical operations
+# Operator	 Description
+# <	         less than
+# <=	       less than or equal to
+# >	         greater than
+# >=	       greater than or equal to
+# ==	       exactly equal to
+# !=	       not equal to
+# !x	       not x
+# x | y	     x OR y
+# x & y	     x AND y
+# isTRUE(x)  test if X is TRUE
+
+
+
+## Assignment operator/extra ----
+
+# Does assignment work in reverse? [left to right]
+
+# TRY THIS
+4 -> y
+
+
+# Important things to note regarding variables:
+# case sensitive (g != G)
+# should not begin with numbers or symbols (5x, %cover)
+# should not contain spaces (fun_games not fun games)
+# make as short and intuitive as possible
+
+
+## Character ----
+
+z <- "we the people"
+
+# check type
+is.character(z)
+
+# both single 'quotes' and "double quotes" work
+is.character('apple')
+
+
+# Compound datatypes --------------------------------------
+
+# Combining simple datatypes into vectors (of the same type) 
+# combining vectors into lists (can be different types here)
+
+## Vectors ----
+
+# Combine 'c()' values into a vector object
+
+k <- c(5, 7, 8, 10)
+
+# see output
+k
+
+# doesn't have to be numbers
+cities <- c("Houston", "New York", "Atlanta", "Seattle")
+
+# Many functions to make numeric vectors
+
+# vector from 2 to 5
+2:5
+
+# Repeats
+rep(c(2, 5), times = 3)
+rep(c(2, 5), each = 3)
+
+rep(c('cat', 'goat'), 2)
+
+## Vectorized operators / functions ----
+
+# What happens here?
+k * 2
+
+# This is the powerful feature called "vectorization"
+
+# what does this do?
+k + k
+k + c(2, 4, 6, 8)
+
+# Explain this output
+k + c(2, 4)
+
+# Now this!
+k + c(1, 2, 3)
+
+
+# We can use logical operators on vectors just like values.
+k <- c(1:10)
+k > 8
+k < 5
+k > 8 | k < 5
+
+# character operations
+
+cities == "Los Angeles"
+
+# What does the error say?
+cities * 2
+
+## Vector to single output functions ----
+
+mean(k)
+sum(k)
+
+
+# More: Simple statistical functions
+# mean (x)               mean of object x
+# mean(x, trim = 0.5)    trimmed mean 
+# sd(x)	                 standard deviation of object x
+# var (x)                variance of object x
+# median(x)	             median of object x
+# quantile(x, probs)	   quantiles where x is the numeric vector whose quantiles are desired and probs is a numeric vector with probabilities in [0,1]. e.g., 30th and 84th percentiles of x: 
+# y <- quantile(x, c(0.3, 0.84))
+# range(x)	             range of object x
+# sum(x), max(x), min(x)  ... etc
+# abs(x)                 absolute value of object x
+
+
+# Getting help for functions -----------------------------
+
+# Option 1:"?function"
+
+?mean
+?sum
+?lm
+# R will tell you if you look for something that doesn't exist:
+?gr
+
+
+## Lists ----
+
+# Lists are the most general, can include everything in them
+# .. including other lists
+small_list <- list(c('small', 'cats'), 4:5)
+
+list(x, k, cities, small_list)
+
+
+## Subseting (indexing) vectors and lists ----
+
+# Get the first element in the list
+small_list[1]
+
+# check type
+small_list[1] %>% class()
+
+# hmm, what does the error say?
+
+# How do we fix it?
+
+
+# Programming -------------------------------------------------
+
+
+## conditionals ----
+
+# Explain this in plain english
+seen <- "something"
+
+if(seen == 'something') {
+  print('say something')
+}
+
+what_is_x <- if(!is.integer(x)) "not an integer"
+
+# check result
+what_is_x
+
+
+## Looping / repeating actions ----
+
+# checking for even numbers
+for(i in k) {
+  if(i %% 2 == 0) {
+    print('even')
+  } else print('odd')
+}
+
+k
+
+## Functions ----
+
+# Functions help write re-usable code chunks
+
+check_even_or_odd <- function(i) {
+  if(i %% 2 == 0) {
+    print('even') 
+  } else print('odd')
+} 
+
+# How to use the function?
+for(i in k) check_even_or_odd(i)
+
+# Now if I change my mind, I can change the function behavior in one place
+
+# MAKE CHANGES IN FUNCTION
+# Change capitalization: 'even' to 'Even' and 'odd' ..
+# Instead of printing, try to store the output; hint, use return()
+
+# Rerun the function here
+
+
+# Can we do this in a vectorized manner? => avoiding the for(..)
+# TRY IT YOURSELF
+
+
+# How to ask for help? ----
+# Reprex function: Running a reproducible example
+# use: for asking help
+
+b <- 5
+if(b %% 2) stop('please help me :(')
+
+# copy the code above and run
+
+reprex::reprex()
+
+# see the results and comment what this function does?
+
+
+
+# Closing thoughts -------------------------------------------
+
+# We walked through simple and compound datatypes in R
+# Saw how we can use conditionals and repeated instructions
+# More efficient implementation with vectorized processing
+
+# Save the r code file (i.e., .R file)
